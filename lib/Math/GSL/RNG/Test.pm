b0c5a28eac6694109f4a003db7fe8c63f366eaa7
Jonathan Leto <leto@polyglot.(none)>
2008-05-08 23:10:01 -0400

    Refactor and add tests to RNG.

--- lib/Math/GSL/RNG/Test.pm
+++ lib/Math/GSL/RNG/Test.pm
@@ -8,27 +8,41 @@ use strict;
 
 sub make_fixture : Test(setup) {
     my $self = shift;
-    $self->{gsl_rng} = Math::GSL::RNG::gsl_rng->new;
+    $self->{rng} = gsl_rng_alloc($gsl_rng_default);
+    gsl_rng_set( $self->{rng}, 1 + 9*(int rand) );
 }
 
 sub teardown : Test(teardown) {
 }
 
-sub GSL_RNG_NEW : Test {
+sub GSL_RNG_NEW : Tests {
     my $self = shift;
-    my $x = $self->{gsl_rng};
-    ok( defined $x && $x->isa('Math::GSL::RNG::gsl_rng'), 'gsl_rng->new' );
+    my $x = $self->{rng};
+    isa_ok( $x, 'Math::GSL::RNG', 'gsl_rng->new' );
 }
 
-sub GSL_RNG_TYPE_DEFAULT : Tests(7) {
+sub GSL_RNG_TYPE : Tests {
+    my $self = shift;
+    my $type = Math::GSL::RNG::gsl_rng_type->new;
+    isa_ok( $type, 'Math::GSL::RNG::gsl_rng_type', 'gsl_rng_type' );
+}
+
+sub GSL_RNG_ALLOC : Tests { 
+    for my $rngtype ( $gsl_rng_random256_bsd, $gsl_rng_knuthran,
+                      $gsl_rng_transputer, $gsl_rng_knuthran2002) {
+        my $rng;
+        eval { $rng = gsl_rng_alloc($rngtype) };
+        isa_ok( $rng, 'Math::GSL::RNG');
+        ok( !$@ );
+    }
+}
+
+sub GSL_RNG_DEFAULT : Tests {
     my $self = shift;
     my $seed = 42;
 
-    my $type = Math::GSL::RNG::gsl_rng_type->new;
-    isa_ok( $type, 'Math::GSL::RNG::gsl_rng_type', 'gsl_rng_type' );
-
     my $rng = gsl_rng_alloc($gsl_rng_default);
-    isa_ok( $rng, 'Math::GSL::RNG', 'gsl_rng_alloc' );
+    isa_ok( $self->{rng}, 'Math::GSL::RNG', 'gsl_rng_alloc' );
 
     eval { gsl_rng_set($rng, $seed) };
     isa_ok( $rng, 'Math::GSL::RNG', 'gsl_rng_set' );

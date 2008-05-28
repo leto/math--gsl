017b3950feaf940d02de32deca6790f6b6b49037
Thierry Moisan <thierry.moisan@gmail.com>
2008-05-27 21:35:59 -0400

    Adding tests to the Complex subsystem.

--- lib/Math/GSL/Complex/Test.pm
+++ lib/Math/GSL/Complex/Test.pm
@@ -43,7 +43,7 @@ sub GSL_COMPLEX_ABS : Tests {
     my $x = Math::GSL::Complex->new(5,3);
     isa_ok( $x, 'Math::GSL::Complex' );
 
-    $abs = gsl_complex_abs($x);
+    my $abs = gsl_complex_abs($x);
     is ( $abs, sqrt(34));
 }
 
@@ -51,8 +51,92 @@ sub GSL_COMPLEX_ABS2 : Tests {
     my $x = Math::GSL::Complex->new(5,3);
     isa_ok( $x, 'Math::GSL::Complex' );
 
-    $abs = gsl_complex_abs2($x);
+    my $abs = gsl_complex_abs2($x);
     is ( $abs, 34);
+}
+
+#sub GSL_COMPLEX_LOGABS : Tests {
+#    my $x = Math::GSL::Complex->new(5,3);
+#    isa_ok( $x, 'Math::GSL::Complex' );
+#
+#    my $abs = gsl_complex_logabs($x);
+#    is ( $abs, log(gsl_complex_abs($x))); #doesn't work, what is supposed to be the output?
+#}
+
+sub GSL_COMPLEX_ADD : Tests {
+    my $x = Math::GSL::Complex->new(5,3);
+    my $y = Math::GSL::Complex->new(1,2);
+    isa_ok( $x, 'Math::GSL::Complex' );
+    isa_ok( $y, 'Math::GSL::Complex' );
+
+    my $abs = gsl_complex_add($x, $y);
+    is ( $abs->real, 6);
+    is ( $abs->imag, 5);
+}
+
+sub GSL_COMPLEX_SUB : Tests {
+    my $x = Math::GSL::Complex->new(5,3);
+    my $y = Math::GSL::Complex->new(1,2);
+    isa_ok( $x, 'Math::GSL::Complex' );
+    isa_ok( $y, 'Math::GSL::Complex' );
+
+    my $abs = gsl_complex_sub($x, $y);
+    is ( $abs->real, 4);
+    is ( $abs->imag, 1);
+}
+
+sub GSL_COMPLEX_MUL : Tests {
+    my $x = Math::GSL::Complex->new(5,3);
+    my $y = Math::GSL::Complex->new(1,2);
+    isa_ok( $x, 'Math::GSL::Complex' ); 
+    isa_ok( $y, 'Math::GSL::Complex' );
+
+    my $abs = gsl_complex_mul($x, $y);
+    is ( $abs->real, 5);
+    is ( $abs->imag, 6);
+}
+
+sub GSL_COMPLEX_DIV : Tests {
+    my $x = Math::GSL::Complex->new(4,5);
+    my $y = Math::GSL::Complex->new(2,1);
+    isa_ok( $x, 'Math::GSL::Complex' );
+    isa_ok( $y, 'Math::GSL::Complex' );
+
+    my $abs = gsl_complex_div($x, $y);
+    is ( $abs->real, 2);
+    is ( $abs->imag, 5);
+}
+
+sub GSL_COMPLEX_ADD_REAL : Tests {
+    my $x = Math::GSL::Complex->new(5,3);
+    isa_ok( $x, 'Math::GSL::Complex' );
+
+    my $abs = gsl_complex_add_real($x, 1);
+    is ( $abs->real, 6);
+}
+
+sub GSL_COMPLEX_SUB_REAL : Tests {
+    my $x = Math::GSL::Complex->new(5,3);
+    isa_ok( $x, 'Math::GSL::Complex' );
+
+    my $abs = gsl_complex_sub_real($x, 1);
+    is ( $abs->real, 4);
+}
+
+sub GSL_COMPLEX_MUL_REAL : Tests {
+    my $x = Math::GSL::Complex->new(5,3);
+    isa_ok( $x, 'Math::GSL::Complex' );
+
+    my $abs = gsl_complex_mul_real($x, 2);
+    is ( $abs->real, 10);
+}
+
+sub GSL_COMPLEX_DIV_REAL : Tests {
+    my $x = Math::GSL::Complex->new(6,3);
+    isa_ok( $x, 'Math::GSL::Complex' );
+
+    my $abs = gsl_complex_div_real($x, 2);
+    is ( $abs->real, 3);
 }
 
 42;

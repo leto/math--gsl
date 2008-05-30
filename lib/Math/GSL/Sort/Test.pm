948c4f42c8a83497f2ec4269d9345d7398b083c4
Thierry Moisan <thierry.moisan@gmail.com>
2008-05-29 22:35:39 -0400

    Adding a test to SF and fixing the Sort crash.

--- lib/Math/GSL/Sort/Test.pm
+++ lib/Math/GSL/Sort/Test.pm
@@ -1,7 +1,7 @@
 package Math::GSL::Sort::Test;
 use base q{Test::Class};
 use Test::More;
-use Math::GSL::Sort qw/:plain :vector :all/;
+use Math::GSL::Sort qw/:all/;
 use Math::GSL qw/is_similar/;
 use Data::Dumper;
 use strict;
@@ -12,11 +12,10 @@ sub teardown : Test(teardown) {
 }
 
 sub GSL_SORT : Tests {
-    my $x = [ 2**15, 1, 42, 17, 6900, 3 ];
-    gsl_sort($x, 1, $#$x + 1 );
-    print Dumper [ $x ];
-    is_deeply ( $x , [ 1, 3, 17 , 42, 6900, 2**15 ], 'gsl_sort' );
-    
+   my $x = [ 2**15, 1, 42, 17, 6900, 3 ];
+   gsl_sort($x, 1, $#$x + 1 );
+   print Dumper [ $x ];
+   is_deeply ( $x , [ 1, 3, 17 , 42, 6900, 2**15 ], 'gsl_sort' );    
 }
 
 42;

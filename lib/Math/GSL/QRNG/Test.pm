8441dc9b80ecdac0a05f4bfe99644d828f8fe4a0
Jonathan Leto <leto@celephais.(none)>
2008-06-22 13:25:26 -0700

    Add some QRNG tests.

--- lib/Math/GSL/QRNG/Test.pm
+++ lib/Math/GSL/QRNG/Test.pm
@@ -8,9 +8,13 @@ use Math::GSL::Errno qw/:all/;
 use strict;
 
 sub make_fixture : Test(setup) {
+    my $self = shift;
+    $self->{sobol} = gsl_qrng_alloc($gsl_qrng_sobol, 2);
 }
 
 sub teardown : Test(teardown) {
+    my $self = shift;
+    gsl_qrng_free($self->{sobol});
 }
 
 sub GSL_QRNG_ALLOC : Tests {
@@ -18,11 +22,33 @@ sub GSL_QRNG_ALLOC : Tests {
     isa_ok( $qrng, 'Math::GSL::QRNG');
 }
 
-sub GSL_QRNG_INIT_GET : Tests { 
-    my $qrng = gsl_qrng_alloc($gsl_qrng_sobol, 2);
-    my ($ok, @values)= gsl_qrng_get($qrng);
+sub GSL_QRNG_STATE_SIZE : Tests {
+    my $self = shift;
+    my $state  = gsl_qrng_state($self->{sobol});
+    my $size   = gsl_qrng_size($self->{sobol});
+    ok( defined $state && $state >0 , 'gsl_qrng_state' );
+    ok( defined $size && $size > 0, 'gsl_qrng_size' );
+}
+
+sub GSL_QRNG_CLONE : Tests {
+    my $self = shift;
+    my $droid = gsl_qrng_clone($self->{sobol});
+    isa_ok($droid, 'Math::GSL::QRNG' );
+}
+
+sub GSL_QRNG_NAME : Tests {
+    my $self = shift;
+    my $name = gsl_qrng_name($self->{sobol});
+    ok ($name eq 'sobol', 'gsl_qrng_name' );
+}
+
+sub GSL_QRNG_GET : Tests { 
+    my $self = shift;
+    my ($ok, @values)= gsl_qrng_get($self->{sobol});
     is ($ok, 0);
-    ok( $values[0] > 0 && $values[0] < 1, "first value");
-#    ok( $values[1] > 0 && $values[1] < 1, "second value");  #this line doesn't work because the typemaps only output one value at the moment...
+    ok( defined $values[0] && $values[0] > 0 && $values[0] < 1, "first value");
+    local $TODO = "the typemaps only output one value at the moment...";
+    ok( defined $values[1] && $values[1] > 0 && $values[1] < 1, "second value"); 
+    ok( $#values = 1, 'gsl_qrng_get returns multiple values' );
 }
 1;

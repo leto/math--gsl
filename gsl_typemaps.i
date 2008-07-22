19fff97010122adaaceb59f0aa2c39e0c9bb1d92
Thierry Moisan <thierry.moisan@gmail.com>
2008-07-21 20:10:56 -0400

    Adding a test to Interp and a new typemap

--- gsl_typemaps.i
+++ gsl_typemaps.i
@@ -22,4 +22,5 @@
 %apply double const [] { double *data, double *f_in, double *f_out, double data[] };
 %apply double const [] { double x[], double a[], double b[] };
 %apply double const [] { const double * x, const double * y };
+%apply double const [] { const double x_array[] };
 
--- lib/Math/GSL/Interp/Test.pm
+++ lib/Math/GSL/Interp/Test.pm
@@ -18,4 +18,34 @@ sub GSL_INTERP_ALLOC : Tests {
  isa_ok($I, 'Math::GSL::Interp');
 }
 
+sub GSL_INTERP_INIT : Tests {
+
+}
+sub GSL_INTERP_BSEARCH : Tests {
+  my $x_array = [ 0.0, 1.0, 2.0, 3.0, 4.0 ];
+  
+  # check an interior point
+  my $index_result = gsl_interp_bsearch($x_array, 1.5, 0, 4);
+  is($index_result, 1);
+
+  # check that we get the last interval if x == last value 
+  $index_result = gsl_interp_bsearch($x_array, 4.0, 0, 4);
+  is($index_result, 3);
+
+  # check that we get the first interval if x == first value
+  $index_result = gsl_interp_bsearch($x_array, 0.0, 0, 4);
+  is($index_result, 0);  
+
+  # check that we get correct interior boundary behaviour 
+  $index_result = gsl_interp_bsearch($x_array, 2.0, 0, 4);
+  is($index_result, 2);
+
+  # check out of bounds above 
+  $index_result = gsl_interp_bsearch($x_array, 10.0, 0, 4);
+  is($index_result, 3);
+
+  # check out of bounds below
+  $index_result = gsl_interp_bsearch($x_array, -10.0, 0, 4);
+  is($index_result, 0);
+}
 1;

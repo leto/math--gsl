63b8e7194826f99fb1f7e541df8463834a0d56ad
Thierry Moisan <thierry.moisan@gmail.com>
2008-07-24 21:05:16 -0400

    Adding a test to Fit

--- Fit.i
+++ Fit.i
@@ -3,7 +3,7 @@
 %include "typemaps.i"
 %include "gsl_typemaps.i"
 
-%apply double *OUTPUT { double * c0, double * c1, double * cov00, double * cov01, double * cov11, double * sumsq };
+%apply double *OUTPUT { double * c0, double * c1, double * cov00, double * cov01, double * cov11, double * sumsq, double * chisq };
 
 %{
 #include "gsl/gsl_fit.h"
--- lib/Math/GSL/Fit/Test.pm
+++ lib/Math/GSL/Fit/Test.pm
@@ -59,5 +59,50 @@ sub GSL_FIT_LINEAR : Tests {
     ok(is_similar_relative($results[4], 0.429796848199937E-03**2, 1e-10));
     ok(is_similar_relative($results[5], 26.6173985294224, 1e-10));
 }
+sub GSL_FIT_WLINEAR : Tests {
+   my @norris_x = (0.2, 337.4, 118.2, 884.6, 10.1, 226.5, 666.3, 996.3,
+                        448.6, 777.0, 558.2, 0.4, 0.6, 775.5, 666.9, 338.0, 
+                        447.5, 11.6, 556.0, 228.1, 995.8, 887.6, 120.2, 0.3, 
+                        0.3, 556.8, 339.1, 887.2, 999.0, 779.0, 11.1, 118.3,
+                        229.2, 669.1, 448.9, 0.5 ) ;
+    my @norris_y = ( 0.1, 338.8, 118.1, 888.0, 9.2, 228.1, 668.5, 998.5,
+                        449.1, 778.9, 559.2, 0.3, 0.1, 778.1, 668.8, 339.3, 
+                        448.9, 10.8, 557.7, 228.3, 998.0, 888.8, 119.6, 0.3, 
+                        0.6, 557.6, 339.3, 888.0, 998.5, 778.9, 10.2, 117.6,
+                        228.9, 668.4, 449.2, 0.2);
+    my $xstride = 2; 
+    my $wstride = 3; 
+    my $ystride = 5;
+    my ($x, $w, $y);
+    for my $i (0 .. 175)
+    {
+        $x->[$i] = 0;
+        $w->[$i] = 0;
+        $y->[$i] = 0;
+    }
 
+    for my $i (0 .. 35)
+    {
+        $x->[$i*$xstride] = $norris_x[$i];
+        $w->[$i*$wstride] = 1.0;
+        $y->[$i*$ystride] = $norris_y[$i];
+    }
+ 
+    my $expected_c0 = -0.262323073774029;
+    my $expected_c1 =  1.00211681802045; 
+    my $expected_cov00 = 6.92384428759429e-02;  # computed from octave
+    my $expected_cov01 = -9.89095016390515e-05; # computed from octave 
+    my $expected_cov11 = 2.35960747164148e-07;  # computed from octave
+    my $expected_sumsq = 26.6173985294224;
+    
+    my @got = gsl_fit_wlinear ($x, $xstride, $w, $wstride, $y, $ystride, 36);
+  
+    ok_status($got[0], $GSL_SUCCESS );
+    ok(is_similar_relative($got[1], $expected_c0, 1e-10), "norris gsl_fit_wlinear c0");
+    ok(is_similar_relative($got[2], $expected_c1, 1e-10), "norris gsl_fit_wlinear c1");
+    ok(is_similar_relative($got[3], $expected_cov00, 1e-10), "norris gsl_fit_wlinear cov00");
+    ok(is_similar_relative($got[4], $expected_cov01, 1e-10), "norris gsl_fit_wlinear cov01");
+    ok(is_similar_relative($got[5], $expected_cov11, 1e-10), "norris gsl_fit_wlinear cov11");
+    ok(is_similar_relative($got[6], $expected_sumsq, 1e-10), "norris gsl_fit_wlinear sumsq");
+}
 1;

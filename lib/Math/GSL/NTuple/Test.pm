5e8fec1ec2cdcbfd3ad4bd3d8577232805cb0129
Thierry Moisan <thierry.moisan@gmail.com>
2008-08-06 10:31:16 -0400

    Adding a failing test to NTuple

--- lib/Math/GSL/NTuple/Test.pm
+++ lib/Math/GSL/NTuple/Test.pm
@@ -3,6 +3,8 @@ use base q{Test::Class};
 use Test::More;
 use Test::Exception;
 use Math::GSL::NTuple qw/:all/; 
+use Math::GSL::Const qw/:all/; 
+use Math::GSL::Errno qw/:all/; 
 use Math::GSL qw/:all/;
 use Data::Dumper;
 use strict;
@@ -16,6 +18,7 @@ sub make_fixture : Test(setup) {
 
 sub teardown : Test(teardown) {
     unlink 'ntuple' if -f 'ntuple';
+    unlink 'ntuple2' if -f 'ntuple2';
 }
 
 sub GSL_NTUPLE_CREATE : Tests {
@@ -30,4 +33,16 @@ sub GSL_NTUPLE_OPEN : Test {
     my $ntuple = gsl_ntuple_open('ntuple',$stuff, $self->{size} );
     isa_ok($ntuple,'Math::GSL::NTuple');
 }
+
+sub GSL_NTUPLE_WRITE_READ : Tests {
+    my $self = shift; 
+    my $base = gsl_ntuple_create('ntuple', [2,1,3],3);
+    
+    my $status = gsl_ntuple_write($base);
+    ok_status($status, $GSL_SUCCESS);
+    my $data = [];
+    my $ntuple = gsl_ntuple_open('ntuple', $data, $self->{size});
+    ok_similar($data, [2,1,3]);
+}
+
 1;

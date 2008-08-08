677506d5ee1afdd451f0ff6aa5ed4d5c5718db9c
Thierry Moisan <thierry.moisan@gmail.com>
2008-08-08 13:47:01 -0400

    Adding documentation and a few tests to ODEIV

--- lib/Math/GSL/ODEIV/Test.pm
+++ lib/Math/GSL/ODEIV/Test.pm
@@ -13,10 +13,26 @@ sub make_fixture : Test(setup) {
 sub teardown : Test(teardown) {
 }
 
+sub GSL_ODEIV_STEP_ALLOC : Tests {
+ my $step = gsl_odeiv_step_alloc($gsl_odeiv_step_rk2, 3);
+ isa_ok( $step, 'Math::GSL::ODEIV' ); 
+}
+
 sub GSL_ODEIV_EVOLVE_ALLOC : Tests {
     my $evolver = gsl_odeiv_evolve_alloc(2);
     isa_ok( $evolver, 'Math::GSL::ODEIV' );
 }
 
+sub NAME : Tests {
+ my $s = gsl_odeiv_step_alloc($gsl_odeiv_step_rk2, 3);
+ my $name = gsl_odeiv_step_name ($s);
+ ok($name eq "rk2");
+}
 
+sub GSL_ODEIV_CONTROL_ALLOC : Tests {
+ my $control_type;
+ local $TODO = "need a way to create a gsl_odeiv_control_type struct";
+# my $control = gsl_odeiv_control_alloc($control_type);
+# isa_ok( $control, 'Math::GSL::ODEIV' );
+}
 1;

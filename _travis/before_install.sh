626bd34a292cad3330fb665a5dec01b645b12ea7
Jonathan "Duke" Leto <jonathan@leto.net>
2016-10-08 14:02:27 -0700

    [travis] Add testing of GSL master branch from @dosten; only download/compile GSL if not already locally cached

--- _travis/before_install.sh
+++ _travis/before_install.sh
@@ -2,18 +2,43 @@
 set -ev
 
 _get_gsl () {
-    wget -q ftp://ftp.gnu.org/gnu/gsl/gsl-$1.tar.gz
-    tar zxpf gsl-$1.tar.gz
-    cd gsl-$1
-    ./configure --prefix /tmp/gsl-$1
-    make -j2
-    make -j2 install
-    cd ..
+    # only download/compile/install if not cached
+    if [ ! -d "gsl-$1" ]; then
+        wget -q ftp://ftp.gnu.org/gnu/gsl/gsl-$1.tar.gz
+        tar zxpf gsl-$1.tar.gz
+        cd gsl-$1
+        ./configure --prefix /tmp/gsl-$1
+        make -j2
+        make -j2 install
+        cd ..
+    fi
 }
 
 get_gsl () {
     _get_gsl $1 &> /dev/null
 }
+
+_get_master_gsl () {
+    if [ ! -d "gsl-master" ]; then
+        git clone git://git.savannah.gnu.org/gsl.git gsl-master
+        cd gsl-master
+    else
+        cd gsl-master
+        git pull origin master
+    fi
+
+    ./autogen.sh
+    ./configure --prefix /tmp/gsl-master
+    make -j2
+    make -j2 install
+    cd ..
+}
+
+get_lastest_gsl () {
+    _get_master_gsl
+    # _get_master_gsl &> /dev/null
+}
+
 
 cpanm -n PkgConfig
 export ORIG_DIR=`pwd`
@@ -24,9 +49,11 @@ get_gsl 1.16
 get_gsl 2.0
 get_gsl 2.1
 get_gsl 2.2.1
+get_master_gsl
+
 ls -la /tmp/gsl-*/lib
 cd /home/travis/build/leto/math--gsl
-PATH=/tmp/gsl-2.2.1/bin:$PATH perl Build.PL &>/dev/null && ./Build &>/dev/null && ./Build dist # create a CPAN dist with latest GSL
+PATH=/tmp/gsl-2.2.1/bin:$PATH perl Build.PL &>/dev/null && ./Build &>/dev/null && ./Build dist # create a CPAN dist with latest supported GSL release
 cp Math-GSL*.tar.gz /tmp
 ls -la /tmp/Math-GSL*.tar.gz # now we have a CPAN dist to test on each version of GSL
 cd $ORIG_DIR

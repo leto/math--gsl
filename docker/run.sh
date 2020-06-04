947f686f956b0b83aaf84829f28bf40902f4e2ee
Håkon Hægland <hakon.hagland@gmail.com>
2020-06-04 08:16:53 +0200

    Fixed argument processing in run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-docker run -it math-gsl-ubuntu-2004 2.6
+if (( $# != 1)) || [[ -z $1 ]] ; then
+    echo "Bad arguments. Please specify version of GSL as first argument"
+    exit
+else
+    echo "Using GSL=$1 ..."
+    GSL_VERSION="$1"
+fi
+
+docker run -it math-gsl-ubuntu-2004 "$GSL_VERSION"
